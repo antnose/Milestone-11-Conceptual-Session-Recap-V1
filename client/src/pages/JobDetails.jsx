9d31a5ec9edaf4d87536e427054a58ca4bf8a135
antnose <antnose28@gmail.com>
2025-05-05 21:22:27 +0600

    complete conceptual sessions part-04 of milestone 11

--- client/src/pages/JobDetails.jsx
+++ client/src/pages/JobDetails.jsx
@@ -1,29 +1,32 @@
+import { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../provider/AuthProvider";
 
 const JobDetails = () => {
-  // const job = useLoaderData();
-  // console.log(job);
+  const job = useLoaderData();
+  const { user } = useContext(AuthContext);
+  const { category, job_title, min_price, max_price, deadline, description } =
+    job || {};
+
   return (
     <div className="flex flex-col md:flex-row justify-around gap-5  items-center min-h-[calc(100vh-306px)] md:max-w-screen-xl mx-auto ">
       {/* Job Details */}
       <div className="flex-1  px-4 py-7 bg-white rounded-md shadow-md md:min-h-[350px]">
         <div className="flex items-center justify-between">
           <span className="text-sm font-light text-gray-800 ">
-            Deadline: 12/08/2024
+            Deadline: {deadline}
           </span>
           <span className="px-4 py-1 text-xs text-blue-800 uppercase bg-blue-200 rounded-full ">
-            Web Development
+            {category}
           </span>
         </div>
 
         <div>
           <h1 className="mt-2 text-3xl font-semibold text-gray-800 ">
-            Build Dynamic Website
+            {job_title}
           </h1>
 
-          <p className="mt-2 text-lg text-gray-600 ">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit...
-          </p>
+          <p className="mt-2 text-lg text-gray-600 ">{description}</p>
           <p className="mt-6 text-sm font-bold text-gray-600 ">
             Buyer Details:
           </p>
@@ -39,7 +42,7 @@ const JobDetails = () => {
             </div>
           </div>
           <p className="mt-6 text-lg font-bold text-gray-600 ">
-            Range: $100 - $150
+            Range: ${min_price} - ${max_price}
           </p>
         </div>
       </div>
@@ -72,6 +75,7 @@ const JobDetails = () => {
                 type="email"
                 name="email"
                 disabled
+                defaultValue={user?.email}
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
               />
             </div>
